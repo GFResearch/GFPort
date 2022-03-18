fe0e4ecaf1c3a60f95f01881523c30acb61bed72
weiwuhao <weiwuhao@blackjack-inc.com>
2022-03-18 12:26:32 +0800

    上传datanode

---
+++ DataNode/Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameFramework.DataNode;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        DataNodeManager dm = new DataNodeManager();
+        var go123 = new GameObject("123");
+        dm.SetData<VarGameObject>("a.b",go123);
+        GameObject r1 = dm.GetData<VarGameObject>("a.b");
+        Debug.LogError(r1.name);
+
+        dm.SetData<VarString>("a.b","aaa");
+        var str=dm.GetData<VarString>("a.b");
+
+        Debug.LogError(str);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
